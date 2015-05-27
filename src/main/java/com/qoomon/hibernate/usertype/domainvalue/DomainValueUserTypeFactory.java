2615140ff16e28b9c7318b61f30c69e6c9521f8e
Bengt Brodersen <qoomon@users.noreply.github.com>
2015-05-27 08:42:36 +0200

    Update DomainValueUserTypeFactory.java

--- src/main/java/com/qoomon/hibernate/usertype/domainvalue/DomainValueUserTypeFactory.java
+++ src/main/java/com/qoomon/hibernate/usertype/domainvalue/DomainValueUserTypeFactory.java
@@ -2,14 +2,14 @@ package com.qoomon.hibernate.usertype.domainvalue;
 
 public class DomainValueUserTypeFactory {
 
-    public <T extends DV<V>, V> DomainValueUserType<T, V> getUserType( Class<T> domainValueType) {
+    public <T extends DV<V>, V> DomainValueUserType<T, V> generate( Class<T> domainValueType) {
         return new DomainValueUserType<T, V>(domainValueType);
     }
 
     public <T extends DV<V>, V> List<DomainValueUserType<T, V>> generate(final Collection<Class<T>> domainValueTypes) {
         final List<DomainValueUserType> result = new LinkedList<DomainValueUserType>();
         for (final Class<T> domainValueType : domainValueTypes) {
-            result.add(getUserType(domainValueType));
+            result.add(generate(domainValueType));
         }
         return result;
     }
